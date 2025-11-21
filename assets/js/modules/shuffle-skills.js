0dbf1019d1b6c24c0a02cc91ba4fe206a37aa09f
Tom Barrasso <319247+Tombarr@users.noreply.github.com>
2025-11-21 14:22:47 -0500

    Skills ellipsis

--- assets/js/modules/shuffle-skills.js
+++ assets/js/modules/shuffle-skills.js
@@ -6,10 +6,22 @@ function getSkills() {
   return Array.from(document.querySelectorAll('.skill-tag'));
 }
 
+function makeLast(allSkills, lastSkillClass) {
+  const skills = [...allSkills];
+  const lastSkillIndex = skills.findIndex(skill => Array.from(skill.classList).includes(lastSkillClass));
+  if (lastSkillIndex === -1) return skills;
+
+  // Make a specific skill the last one
+  const [lastSkill] = skills.splice(lastSkillIndex, 1);
+  skills.push(lastSkill);
+  return skills;
+}
+
 export function initShuffleSkills() {
   if (!hasReplaceChildren) return;
   const skills = getSkills();
   if (skills.length === 0) return;
 
-  skills[0].parentNode.replaceChildren(...shuffle(skills));
+  const shuffledSkills = makeLast(shuffle(skills), 'ellipsis');
+  skills[0].parentNode.replaceChildren(...shuffledSkills);
 }
